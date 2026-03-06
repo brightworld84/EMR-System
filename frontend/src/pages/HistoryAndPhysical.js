cd304b5423d2f1106c5815a4d048e655ab4e8930
Michael Fulbright <fulbrightdigital@gmail.com>
2026-03-06 13:26:09 -0600

    Fix HistoryAndPhysical: add useRef guard to prevent duplicate POST on mount

--- frontend/src/pages/HistoryAndPhysical.js
+++ frontend/src/pages/HistoryAndPhysical.js
@@ -154,7 +154,11 @@ function HistoryAndPhysical() {
     }
   };
 
+  const loadStarted = useRef(false);
+
   useEffect(() => {
+    if (loadStarted.current) return;
+    loadStarted.current = true;
     loadOrCreate();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [checkinId]);
